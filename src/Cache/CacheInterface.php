2da6dab402c6f282bc93873d61a716cea29ad609
Jordan Gosney <wizofgoz2@gmail.com>
2017-02-06 17:18:41 +0000

    Apply fixes from StyleCI

--- src/Cache/CacheInterface.php
+++ src/Cache/CacheInterface.php
@@ -22,13 +22,18 @@
 *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 *	SOFTWARE.
 */
+
 namespace Crester\Cache;
 
 interface CacheInterface
 {
-	public function __construct(array $Config);
-	public function crestCheck($Route, $Args);
-	public function crestUpdate($Route, $Args, $Response);
-	public function xmlCheck($AccessCode, $AccessType, $Scope, $EndPoint, $Args);
-	public function xmlUpdate($Data, $CachedUntil, $AccessCode, $AccessType, $Scope, $EndPoint, $Args);
+    public function __construct(array $Config);
+
+    public function crestCheck($Route, $Args);
+
+    public function crestUpdate($Route, $Args, $Response);
+
+    public function xmlCheck($AccessCode, $AccessType, $Scope, $EndPoint, $Args);
+
+    public function xmlUpdate($Data, $CachedUntil, $AccessCode, $AccessType, $Scope, $EndPoint, $Args);
 }
--- src/Core/CRESTBase.php
+++ src/Core/CRESTBase.php
@@ -22,56 +22,57 @@
 *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 *	SOFTWARE.
 */
+
 namespace Crester\Core;
 
 abstract class CRESTBase
 {
-	/*
-	*	URL to root of CREST API (Authenticated)
-	*
-	*	@var string
-	*/
-	const CREST_AUTH_ROOT = 'https://crest-tq.eveonline.com/';
+    /*
+    *	URL to root of CREST API (Authenticated)
+    *
+    *	@var string
+    */
+    const CREST_AUTH_ROOT = 'https://crest-tq.eveonline.com/';
 
-	/*
-	*	URL to root of CREST API (Unauthenticated - depreciated)
-	*
-	*	@var string
-	*/
-	const CREST_PUBLIC_ROOT = 'https://public-crest.eveonline.com/';
+    /*
+    *	URL to root of CREST API (Unauthenticated - depreciated)
+    *
+    *	@var string
+    */
+    const CREST_PUBLIC_ROOT = 'https://public-crest.eveonline.com/';
 
-	/*
-	*	URL for API authentication
-	*
-	*	@var string
-	*/
-	const CREST_LOGIN = 'https://login.eveonline.com/oauth/token';
+    /*
+    *	URL for API authentication
+    *
+    *	@var string
+    */
+    const CREST_LOGIN = 'https://login.eveonline.com/oauth/token';
 
-	/*
-	*	URL for verifying login and getting character information
-	*
-	*	@var string
-	*/
-	const CREST_VERIFY = 'https://login.eveonline.com/oauth/verify';
+    /*
+    *	URL for verifying login and getting character information
+    *
+    *	@var string
+    */
+    const CREST_VERIFY = 'https://login.eveonline.com/oauth/verify';
 
-	/*
-	*	Constant for designating a call follows the Bearer Token authentication schema
-	*
-	*	@var integer
-	*/
-	const AUTHORIZATION_BEARER = 0;
+    /*
+    *	Constant for designating a call follows the Bearer Token authentication schema
+    *
+    *	@var integer
+    */
+    const AUTHORIZATION_BEARER = 0;
 
-	/*
-	*	Constant for designating a call follows the Basic authentication schema
-	*
-	*	@var integer
-	*/
-	const AUTHORIZATION_BASIC = 1;
+    /*
+    *	Constant for designating a call follows the Basic authentication schema
+    *
+    *	@var integer
+    */
+    const AUTHORIZATION_BASIC = 1;
 
-	/*
-	*	Constant for designating a call requires no authorization
-	*
-	*	@var integer
-	*/
-	const AUTHORIZATION_NONE = 2;
+    /*
+    *	Constant for designating a call requires no authorization
+    *
+    *	@var integer
+    */
+    const AUTHORIZATION_NONE = 2;
 }
